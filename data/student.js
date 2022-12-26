bb9e4910a16f26b0dd4ae729a4eff8a8226654b8
Faqih Azhar <faqihazh@gmail.com>
2022-12-26 18:45:31 +0700

    Update & Delete

---
+++ data/student.js
@@ -0,0 +1,156 @@
+// TODO 1: Buat data students
+const student = [
+  {
+    nama: "Avie Barthorpe",
+    nim: "0110332376",
+    email: "abarthorpe0@trellian.com",
+    jurusan: "Sistem Informasi",
+  },
+  {
+    nama: "Norry Mathivon",
+    nim: "0110332376",
+    email: "nmathivon1@dmoz.org",
+    jurusan: "Sistem Informasi",
+  },
+  {
+    nama: "Delores Treadger",
+    nim: "0110332376",
+    email: "dtreadger2@desdev.cn",
+    jurusan: "Sistem Informasi",
+  },
+  {
+    nama: "Robinia Nassau",
+    nim: "0110332376",
+    email: "rvonnassau3@bloomberg.com",
+    jurusan: "Sistem Informasi",
+  },
+  {
+    nama: "Nickolas Baress",
+    nim: "0110332376",
+    email: "nbaress4@dyndns.org",
+    jurusan: "Teknik Informatika",
+  },
+  {
+    nama: "Alethea Mulholland",
+    nim: "0110332376",
+    email: "amulholland5@chron.com",
+    jurusan: "Sistem Informasi",
+  },
+  {
+    nama: "Keith Clapham",
+    nim: "0110332376",
+    email: "kclapham6@github.io",
+    jurusan: "Teknik Informatika",
+  },
+  {
+    nama: "Judi Chamberlain",
+    nim: "0110332376",
+    email: "jchamberlain7@exblog.jp",
+    jurusan: "Sistem Informasi",
+  },
+  {
+    nama: "Freddy Roan",
+    nim: "0110332376",
+    email: "froan8@dmoz.org",
+    jurusan: "Teknik Informatika",
+  },
+  {
+    nama: "Gracie Egan",
+    nim: "0110332376",
+    email: "gegan9@digg.com",
+    jurusan: "Sistem Informasi",
+  },
+  {
+    nama: "Muffin Butler",
+    nim: "0110332376",
+    email: "mbutlera@mozilla.org",
+    jurusan: "Teknik Informatika",
+  },
+  {
+    nama: "Jodi Petrolli",
+    nim: "0110332376",
+    email: "jpetrollib@myspace.com",
+    jurusan: "Sistem Informasi",
+  },
+  {
+    nama: "Augustin Trahmel",
+    nim: "0110332376",
+    email: "atrahmelc@usnews.com",
+    jurusan: "Teknik Informatika",
+  },
+  {
+    nama: "Nada Normanton",
+    nim: "0110332376",
+    email: "nnormantond@godaddy.com",
+    jurusan: "Sistem Informasi",
+  },
+  {
+    nama: "Teresina Leete",
+    nim: "0110332376",
+    email: "tleetee@qq.com",
+    jurusan: "Sistem Informasi",
+  },
+  {
+    nama: "Paulo Gallgher",
+    nim: "0110332376",
+    email: "pgallgherf@altervista.org",
+    jurusan: "Teknik Informatika",
+  },
+  {
+    nama: "Sargent Clabburn",
+    nim: "0110332376",
+    email: "sclabburng@cbslocal.com",
+    jurusan: "Teknik Informatika",
+  },
+  {
+    nama: "Niven Btham",
+    nim: "0110332376",
+    email: "nbthamh@prweb.com",
+    jurusan: "Teknik Informatika",
+  },
+  {
+    nama: "Dela Fleisch",
+    nim: "0110332376",
+    email: "dfleischi@thetimes.co.uk",
+    jurusan: "Sistem Informasi",
+  },
+  {
+    nama: "Ralph Itzkin",
+    nim: "0110332376",
+    email: "ritzkinj@godaddy.com",
+    jurusan: "Teknik Informatika",
+  },
+  {
+    nama: "Chip Nardrup",
+    nim: "0110332376",
+    email: "cnardrupk@whitehouse.gov",
+    jurusan: "Teknik Informatika",
+  },
+  {
+    nama: "Jaquelyn Merrison",
+    nim: "0110332376",
+    email: "jmerrisonl@linkedin.com",
+    jurusan: "Sistem Informasi",
+  },
+  {
+    nama: "Karel Hopfner",
+    nim: "0110332376",
+    email: "khopfnerm@webs.com",
+    jurusan: "Sistem Informasi",
+  },
+  {
+    nama: "Zandra Maud",
+    nim: "0110332376",
+    email: "zmaudn@dedecms.com",
+    jurusan: "Sistem Informasi",
+  },
+  {
+    nama: "Esra Hatz",
+    nim: "0110332376",
+    email: "ehatzo@tinypic.com",
+    jurusan: "Teknik Informatika",
+  },
+];
+
+// TODO 2: export data students
+module.exports = student;
